d7094095759962c53ad44bad89c2b1719318ad15
Daniel <Daeynasvistas@gmail.com>
2017-01-13 11:30:58 +0000

    2 commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(teste_git.Startup))]
+namespace teste_git
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
